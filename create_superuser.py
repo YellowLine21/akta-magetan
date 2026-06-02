5ccf11b2d8cbcb70e488ebd182be1b9b75e1b8b5
YellowLine21 <24051204247@mhs.unesa.ac.id>
2026-06-02 18:56:29 +0700

    Update create_superuser.py

--- create_superuser.py
+++ create_superuser.py
@@ -8,7 +8,7 @@ from django.contrib.auth import get_user_model
 
 User = get_user_model()
 
-nik = os.getenv('SUPERUSER_NIK', '0000000000000000')
+nik = os.getenv('SUPERUSER_NIK', '1234567890123456')
 email = os.getenv('SUPERUSER_EMAIL', 'admin@example.com')
 password = os.getenv('SUPERUSER_PASSWORD', 'admin123')
 nama = os.getenv('SUPERUSER_USERNAME', 'Admin')
