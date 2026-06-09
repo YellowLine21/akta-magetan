a7760f526454de779b9e3d197601f02603adc455
YellowLine21 <24051204247@mhs.unesa.ac.id>
2026-06-10 01:52:57 +0700

    Update settings.py

--- akta_magetan/settings.py
+++ akta_magetan/settings.py
@@ -134,7 +134,7 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 # Jangan hardcode credentials di sini.
 EMAIL_BACKEND      = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST         = 'smtp.gmail.com'
-EMAIL_PORT         = 587
+EMAIL_PORT         = 465
 EMAIL_USE_TLS      = True
 EMAIL_USE_SSL      = False
 EMAIL_HOST_USER    = os.environ.get('EMAIL_HOST_USER', '')
